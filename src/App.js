b3f9387cf4204884c853b356fb210f2f5bcd1c86
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-12 03:45:14 +0000

    home route added to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {Redirect, Route, Switch} from 'react-router-dom'
 
 import './App.css'
 import Login from './components/Login'
+import Home from './components/Home'
 import NotFound from './components/NotFound'
 
 //  TODO: add Routing
 const App = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
+    <Route exact path="/" component={Home} />
     <Route path="/not-found" component={NotFound} />
     <Redirect to="not-found" component={NotFound} />
   </Switch>
