f12a9bf7794b60760cb6fa366c0fd35856793f74
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-12 03:01:01 +0000

    Not found Route added to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import {Redirect, Route, Switch} from 'react-router-dom'
 
 import './App.css'
 import Login from './components/Login'
+import NotFound from './components/NotFound'
 
-// write your code here
 //  TODO: add Routing
 const App = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
-    {/* <Redirect to='not-found' component={NotFound}/> */}
+    <Redirect to="not-found" component={NotFound} />
   </Switch>
 )
 
