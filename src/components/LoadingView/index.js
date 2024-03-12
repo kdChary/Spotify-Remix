b344ba9fdea9112331a0e9af0f4be9a672d9e1af
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-12 16:50:54 +0000

    loading view styled-2

--- src/components/LoadingView/index.js
+++ src/components/LoadingView/index.js
@@ -10,7 +10,13 @@ const LoadingView = () => {
       <img src={imgUrl} alt="website logo" className="loading-section-logo" />
       <div className="loader-text-section" data-testid="loader">
         <h1 className="loader-text">Loading</h1>
-        <Loader type="ThreeDots" color="#ffffff" height="30" width="30" />
+        <Loader
+          type="ThreeDots"
+          color="#ffffff"
+          height="39"
+          width="39"
+          className="loader"
+        />
       </div>
     </div>
   )
