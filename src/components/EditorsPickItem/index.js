72a69d6319b900f900b54a9bc3d4c14853c01632
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-12 17:06:56 +0000

    editorsPickItem coded

---
+++ src/components/EditorsPickItem/index.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+// TODO: add protected route
+
+const EditorsPickItem = props => {
+  const {itemDetails} = props
+  const {name, id, imageUrl} = itemDetails
+
+  return (
+    <li className="editors-pick-item">
+      <Link to={`/playlist/${id}`} className="link-item">
+        <img src={imageUrl} alt="" className="playlist-img" />
+        <p className="playlist-text">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default EditorsPickItem
